52b70f26b0ae194fb50d79d3a7f9d8d2c510b7fe
TddyJ <157575715+TddyJ@users.noreply.github.com>
2024-05-18 00:58:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -50,17 +50,15 @@ if page == "Home":
     file = st.file_uploader("Choose an image of an ant among the following species: Fire Ant, Ghost Ant, Little Black Ant, Weaver Ant", type=["jpg", "png"])
     
     # Function to make predictions
-    def import_and_predict(image_path, model):
-        size = (150, 150)  # Target size for the image
-        img = image.load_img(image_path, target_size=size)  # Load the image with the target size
-        img_array = image.img_to_array(img)  # Convert the image to an array
-        img_array = np.expand_dims(img_array, axis=0)  # Expand dimensions to fit the model's input shape
-        img_array = img_array / 255.0  # Normalize the image
+    def import_and_predict(image_data, model):
+        size = (150, 150)  
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Ensure the image is resized correctly
+        img = np.asarray(image)  # Convert the image to a NumPy array
+        img = img / 255.0  # Normalize the image
+        img = np.expand_dims(img, axis=0)  # Add a batch dimension
 
-        prediction = model.predict(img_array)  # Make prediction
-        predicted_class = np.argmax(prediction)  # Get the class with the highest probability
-
-        return predicted_class
+        prediction = model.predict(img)  # Make prediction
+        return prediction
     
     if file is None:
         st.text("Please upload an image file")
